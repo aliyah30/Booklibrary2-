4c5e64f4b079c37b6e42a7a942910065e0f0a22b
Benny (RZ1) <149847536+RZed01@users.noreply.github.com>
2025-04-30 14:47:00 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from '.app.config';
+import { AppComponent } from '.app.component';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
