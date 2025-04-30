71097e7efcab2b4b374a795eccfd021cca246820
Benny (RZ1) <149847536+RZed01@users.noreply.github.com>
2025-04-30 14:45:33 -0400

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './profile/profile.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
+import { ProfileComponent } from './profile.component';
+import { BookDetailComponent } from './book-detail.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
