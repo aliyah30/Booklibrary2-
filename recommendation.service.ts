96aaa769a51fb7429d3d35b23ffcd641ddbd9869
Benny (RZ1) <149847536+RZed01@users.noreply.github.com>
2025-04-30 11:50:30 -0400

    Update recommendation.service.ts
    Updated rest of code to run client side

--- recommendation.service.ts
+++ recommendation.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, forkJoin, map } from 'rxjs';
 import { Book } from '../book-detail/book.model';
 import { BookService } from '../book-detail/book.service';
@@ -7,64 +6,63 @@ import { BookService } from '../book-detail/book.service';
 @Injectable({
   providedIn: 'root'
 })
-export class RecommendationService;
+export class RecommendationService {
+  constructor(private bookService: BookService) {}
 
-  constructor(
-    private http: HttpClient,
-    private bookService: BookService
-  ) {}
+  getRecommendations(borrowedBooks: Book[]): Observable<{
+    byAuthor: Book[],
+    byDate: Book[],
+    similar: Book[]
+  }> {
+    return forkJoin({
+      allBooks: this.bookService.getBooks(),
+      similar: this.getRecommendationsByHistory(borrowedBooks)
+    }).pipe(
+      map(({ allBooks, similar }) => ({
+        byAuthor: this.getBooksByAuthors(allBooks, borrowedBooks),
+        byDate: this.getBooksByDate(allBooks, borrowedBooks),
+        similar
+      }))
+    );
+  }
 
-getRecommendations(borrowedBooks: Book[]): Observable<{
-  byAuthor: Book[],
-  byDate: Book[],
-  similar: Book[]
-}> {
-  return this.bookService.getBooks().pipe(
-    map(allBooks => {
-      const byAuthor = this.getBooksByAuthors(allBooks, borrowedBooks);
-      const byDate = this.getBooksByDate(allBooks, borrowedBooks);
-      const similar = this.getRecommendationsByHistory(allBooks, borrowedBooks);
-      return { byAuthor, byDate, similar };
-    })
-  );
-}
-
-private getRecommendationsByHistory(allBooks: Book[], borrowedBooks: Book[]): Book[] {
-  return allBooks.filter(book =>
-    !borrowedBooks.find(b => b.book_id === book.book_id)
-  ).slice(0, 3);
-}
+  private getRecommendationsByHistory(borrowedBooks: Book[]): Observable<Book[]> {
+    return this.bookService.getBooks().pipe(
+      map(books =>
+        books
+          .filter(book => !borrowedBooks.some(b => b.book_id === book.book_id))
+          .slice(0, 3)
+      )
+    );
+  }
 
   private getBooksByAuthors(allBooks: Book[], borrowedBooks: Book[]): Book[] {
-    const authors = new Set(borrowedBooks.map(book => book.author));
-    return allBooks.filter(book => 
-      authors.has(book.author) && 
-      !borrowedBooks.find(b => b.book_id === book.book_id)
-    ).slice(0, 3);
+    const authors = new Set(borrowedBooks.map(b => b.author));
+    return allBooks
+      .filter(book =>
+        authors.has(book.author) &&
+        !borrowedBooks.some(b => b.book_id === book.book_id)
+      )
+      .slice(0, 3);
   }
 
   private getBooksByDate(allBooks: Book[], borrowedBooks: Book[]): Book[] {
-    const averageDate = this.getAveragePublicationDate(borrowedBooks);
+    const avgDate = this.getAveragePublicationDate(borrowedBooks);
     return allBooks
-      .filter(book => 
-        !borrowedBooks.find(b => b.book_id === book.book_id)
-      )
+      .filter(book => !borrowedBooks.some(b => b.book_id === book.book_id))
       .sort((a, b) => {
-        const dateA = new Date(a.publication_date || '');
-        const dateB = new Date(b.publication_date || '');
-        const diffA = Math.abs(dateA.getTime() - averageDate.getTime());
-        const diffB = Math.abs(dateB.getTime() - averageDate.getTime());
-        return diffA - diffB;
+        const da = new Date(a.publication_date).getTime();
+        const db = new Date(b.publication_date).getTime();
+        return Math.abs(da - avgDate.getTime()) - Math.abs(db - avgDate.getTime());
       })
       .slice(0, 3);
   }
 
   private getAveragePublicationDate(books: Book[]): Date {
-    const dates = books
-      .map(book => new Date(book.publication_date || ''))
-      .filter(date => !isNaN(date.getTime()));
-    
-    const totalTime = dates.reduce((sum, date) => sum + date.getTime(), 0);
-    return new Date(totalTime / dates.length);
+    const times = books
+      .map(b => new Date(b.publication_date).getTime())
+      .filter(t => !isNaN(t));
+    const avg = times.reduce((sum, t) => sum + t, 0) / times.length;
+    return new Date(avg);
   }
 }
