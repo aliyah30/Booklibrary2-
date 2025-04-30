0db461908a13537ddbe6b422c485eac568c34ea6
Benny (RZ1) <149847536+RZed01@users.noreply.github.com>
2025-04-30 12:34:56 -0400

    Update book.service.ts

--- book.service.ts
+++ book.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Book ) from './book.model';
+import { Book } from './book.model';
 
 @Injectable({
   providedIn: 'root'
